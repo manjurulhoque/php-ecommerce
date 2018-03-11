ff40a5962d9f7e65eaba4f2ba62069a9b414767c
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-03-12 00:09:02 +0600

    admin login

--- includes/session.php
+++ includes/session.php
@@ -3,12 +3,15 @@
 class Session
 {
     private $signed_in;
+    private $admin_signed_in;
     public $user_id;
+    public $admin_user_id;
     public $count;
 
     function __construct()
     {
         session_start();
+        $this->check_the_admin_login();
         $this->check_the_login();
     }
 
@@ -17,9 +20,28 @@ class Session
         return $this->signed_in;
     }
 
+    public function admin_signed_in()
+    {
+        return $this->admin_signed_in;
+    }
+
     public function get_user()
     {
         return $_SESSION['user'];
+    }
+
+    public function get_admin()
+    {
+        return $_SESSION['admin'];
+    }
+
+    public function login_admin($user)
+    {
+        if ($user) {
+            $_SESSION['admin'] = $user;
+            $this->admin_user_id = $_SESSION['admin_user_id'] = $user->id;
+            $this->admin_signed_in = true;
+        }
     }
 
     public function login($user)
@@ -45,6 +67,24 @@ class Session
         unset($_SESSION['user_id']);
         unset($this->user_id);
         $this->signed_in = false;
+    }
+
+    public function logout_admin()
+    {
+        unset($_SESSION['admin_user_id']);
+        unset($this->admin_user_id);
+        $this->admin_signed_in = false;
+    }
+
+    private function check_the_admin_login()
+    {
+        if (isset($_SESSION['admin_user_id'])) {
+            $this->admin_user_id = $_SESSION['admin_user_id'];
+            $this->admin_signed_in = true;
+        } else {
+            unset($this->admin_user_id);
+            $this->admin_signed_in = false;
+        }
     }
 
     private function check_the_login()
